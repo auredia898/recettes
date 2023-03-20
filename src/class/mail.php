e343304a6df6a2c6c3e1ad0d5b1c964968dd5fca
yessirine <yessirinelove@gmail.com>
2023-03-20 02:02:57 +0100

    mot de passe oublé 1

---
+++ src/class/mail.php
@@ -0,0 +1,27 @@
+<?php
+  require 'vendor/autoload.php';
+  use \Mailjet\Resources;
+  $mj = new \Mailjet\Client('****************************1234','****************************abcd',true,['version' => 'v3.1']);
+  $body = [
+    'Messages' => [
+      [
+        'From' => [
+          'Email' => "yessirinelove@gmail.com",
+          'Name' => "Yessirine"
+        ],
+        'To' => [
+          [
+            'Email' => "yessirinelove@gmail.com",
+            'Name' => "Yessirine"
+          ]
+        ],
+        'Subject' => "Greetings from Mailjet.",
+        'TextPart' => "My first Mailjet email",
+        'HTMLPart' => "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+        'CustomID' => "AppGettingStartedTest"
+      ]
+    ]
+  ];
+  $response = $mj->post(Resources::$Email, ['body' => $body]);
+  $response->success() && var_dump($response->getData());
+?>
