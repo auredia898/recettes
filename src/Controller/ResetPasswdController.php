5e62ada1c46e8340c7e5cca23866f71d74823bd2
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-03-24 00:26:44 +0100

    reset password form

--- src/Controller/ResetPasswdController.php
+++ src/Controller/ResetPasswdController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Class\Mail;
+use App\Entity\Client;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\ResetPassword;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,58 +11,81 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ResetPasswdController extends AbstractController
 {
     private $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
-        $this ->entityManager = $entityManager;
+        $this->entityManager = $entityManager;
     }
 
-    #[Route('/reset', name: 'reset')]
-    public function index(Request $request,UserPasswordHasherInterface $hasher ): Response
+    #[Route("/forgot_password", name: 'reset_password')]
+    public function index(Request $request)
     {
-        //initialization of variables
-        $notification = null;
-        $client = new Client();
-        $form = $this->createForm(RegisterFormType::class, $client);
-        $form ->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $client = $form->getData();
-            $token = $form->getData();
-            $search_mail = $this->entityManager->getRepository(Client::class)->findOneByEmail($client->getEmail());
-            $search_token = $this->entityManager->getRepository(ResetPassword::class)->findOneByEmail($token->getToken());
-
-            //check if the email already exists in the database
-            if ($search_mail = $client && $search_token=$token) {
-                //$password = $hasher->hashPassword($client, $client->getpassword());
-                //$client->setpassword($password);
-
-                $this->entityManager->persist($client);
-                $this->entityManager->flush();
-                //send a welcome message by email to the user when their account is created
-                $mail = new Mail();
-                $title = "Message de bienvenu";
-                $subtitle = "Profitez de nos sites touristiques et culturels.";
-                $content="Bonjour".$client->getFirstName()."<br/>Bienvenu sur notre site de tourisme.";
-                $mail->send($client->getEmail(), $client->getFirstName(), 'Bienvenue sur notre site de tourisme', $content, $title, $subtitle);
-                $notification = 'sucess';
-
-            } else {
-                $notification = "error";
-            }
-            //redirect user in home page after registration
+        if($this->getUser())
+        {
             return $this->redirectToRoute('home');
         }
-        return $this->render('reset_passwd/index.html.twig', [
-            'controller_name' => 'ResetPasswdController',
-            'form' => $form->createView(),
-            'notification' => $notification,
-        ]);
+
+        if ($request->get('email')) {
+
+            $client = $this->entityManager->getRepository(Client::class)->findOneByEmail($request->get('email'));
+
+            if ($client) {
+                $reset_password = new ResetPassword();
+                $reset_password ->setUser($client);
+                $reset_password->setToken(uniqid());
+                $reset_password->setCreatedAt(new \DateTime());
+                $this->entityManager->persist($reset_password);
+                $this->entityManager->flush();
+
+                $url = $this->generateUrl('update_password', [
+                    'token' => $reset_password->getToken()
+                ]);
+                $content= "Bonjour".$client->getFirstname()."<br/>Vous avez demandé a réinitialiser votre mot de passe sur la Boutique Store.<br/><br/>";
+                $content ="Merci de bien vouloir <a href".$url.">cliquer ici</a> pour mettre à jour votre mot de passe";
+                $mail = new mail();
+                $mail->send($client->getEmail(), $client->getFirstname().' '.$client->getLastname(), 'Réinitialiser votre mot de passe sur Store', $content);
+                $this->addFlash('notice', 'Veuillez consulter votre email.');
+            }
+            else{
+                $this->addFlash('notice', 'Adresse email inconnue');
+            }
+        }
+        return $this->render('reset_password/index.html.twig');
     }
 
-}
-    
+    #[Route("/change_password/{token}", name: 'update_password')]
+    public function update(Request $request, $token, UserPasswordEncoder $encoder)
+    {
+        $reset_password = $this->entityManager->getRepository(ResetPassword::class)->findOneByToken($token);
 
+        if (!$reset_password) {
+            return $this->redirectToRoute('reset_password');
+        }
+        $now = new DateTime();
+        if ($now > $reset_password->getCreatedAt()->modify('+1hour')) {
+            $this->addFlash('notice', 'Votre demande a expiré');
+            return $this->redirectToRoute('reset_password');
+        }
+
+        $form = $this->createForm(ResetPasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $new_pwd = $form->get('new_password')->getData();
+            $password = $encoder->encodePassword($reset_password->getUser(), $new_pwd);
+            $reset_password->getUser()->setPassword($password);
+            $this->entityManager->flush();
+            $this->addFlash('notice','Votre mot de passe a bien été mise à jour');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('reset_password/update.html.twig',[
+            'form'=> $form->createView()
+        ]);
+    }
+}
