e2f8e7d110f665b6d20d4e6582ff22e9479e8f0e
yessirine <yessirinelove@gmail.com>
2023-04-01 23:39:31 +0200

    liens

--- Tuteuré/animation.js
+++ Tuteuré/animation.js
@@ -12,12 +12,51 @@ document.querySelectorAll(".nav-link").forEach((link) =>
     navMenu.classList.remove("active");
   })
 );
+function LignePanier (code, qte, prix)
+{
+    this.codeArticle = code;
+    this.qteArticle = qte;
+    this.prixArticle = prix;
+    this.ajouterQte = function(qte)
+    {
+        this.qteArticle += qte;
+    }
+    this.getPrixLigne = function()
+    {
+        var resultat = this.prixArticle * this.qteArticle;
+        return resultat;
+    }
+    this.getCode = function() 
+    {
+        return this.codeArticle;
+    }
+};
+function Panier()
+{
+    this.liste = [];
+    this.ajouterArticle = function(code, qte, prix)
+    { 
+        var index = this.getArticle(code);
+        if (index == -1) this.liste.push(new LignePanier(code, qte, prix));
+        else this.liste[index].ajouterQte(qte);
+    }
+    this.getPrixPanier = function()
+    {
+        var total = 0;
+        for(var i = 0 ; i < this.liste.length ; i++)
+            total += this.liste[i].getPrixLigne();
+        return total;
+    }
+    this.getArticle = function(code)
+    {
+        for(var i = 0 ; i <this.liste.length ; i++)
+            if (code == this.liste[i].getCode()) return i;
+        return -1;
+    }
+    this.supprimerArticle = function(code)
+    {
+        var index = this.getArticle(code);
+        if (index > -1) this.liste.splice(index, 1);
+    }
+}
 
-    var typed = new Typed(".multiple-text", {
-    strings: ["Le Benin, comme nulle part ailleurs.", "Le pays qui voyage en vous.",
-     "Destination Benin, prenez le temps de tout vivre !"],
-        typeSpeed:100,
-        backSpeed:100,
-        backDelay:1000,
-        loop: true
-})
